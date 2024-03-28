4b3a654ff54ec4dd00b6128dd72b55a115891638
Sagyndyk <sagindik2003@gmail.com>
2024-03-28 18:54:17 +0500

    reginster

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -12,13 +12,46 @@ export const useAuthStore = defineStore({
     getters: {},
 
     actions: {
+        async login (data) {
+          try {
+              this.loading = true
+              let response = await api.post('login', data)
+
+              if (response.status === 200) {
+                  notify({
+                      text: response.data.message,
+                      type: 'success'
+                  })
+                  sessionStorage.setItem('token', response.data.data.token)
+                  sessionStorage.setItem('user', response.data.data.user)
+                  router.push({name: 'HomePage'})
+              }
+          } catch (e) {
+              const errorData = e?.response?.data?.data
+
+              if (e?.response?.status === 403 && errorData) {
+                  for (const key in errorData) {
+                      notify({
+                          title: 'Валидация',
+                          text: errorData[key],
+                          type: 'error'
+                      })
+                  }
+              } else {
+                  notify({
+                      text: 'При регистрации произошло ошибка!!!',
+                      type: 'error'
+                  })
+              }
+          } finally {
+              this.loading = false
+          }
+        },
         async register (data) {
             try {
                 this.loading = true
-                console.log(data)
 
                 let response = await api.post('register', data)
-
 
                 if (response.status === 200) {
                     notify({
